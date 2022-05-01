bdccb4d42b07a3c68697eada08de6fc77094b1fd
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-05-02 01:49:11 +0530

    conditional rendering with cart

--- front-end/src/Components/Pages/CartPage.jsx
+++ front-end/src/Components/Pages/CartPage.jsx
@@ -7,7 +7,8 @@ import { deleteFromCart } from '../../Redux/Action/cartPageAction';
 
 
 const CartPage = () => {
-    const [total, setTotal] = useState(0)
+    const [totalOriginalPrice, setTotalOriginalPrice] = useState(0);
+    const [discounted_price, setDiscountedPrice] = useState(0);
     const selector = useSelector((state) => state.cartPageReducer.cartData);
     console.log(selector);
     console.log(selector.length);
@@ -22,55 +23,60 @@ const CartPage = () => {
 
 
     const fun_total = () => {
-        let sum = 0;
+        let sumOriginalPrice = 0;
+        let sumDiscountedPrice = 0;
         for (let i = 0; i < selector.length; i++) {
-            sum += selector[i].discounted_price;
+            sumOriginalPrice += selector[i].original_price;
+            sumDiscountedPrice += selector[i].discounted_price;
         }
-        setTotal(sum);
+        setTotalOriginalPrice(sumOriginalPrice);
+        setDiscountedPrice(sumDiscountedPrice);
     }
 
     useEffect(() => {
         fun_total();
     }, [selector]);
 
-
-
-
-
-
-
-
     return <>
         <div style={{ marginTop: "10%", fontSize: 'large', fontWeight: 'bolder', marginLeft: '2%', marginRight: '2%' }} >
             <h1></h1>
         </div>
-        <div style={{ width: "50%", margin: "auto", }}>
-            {selector.map((e) => {
-                return <>
+        <div style={{ display: "flex", justifyContent: 'center' }}>
+            <div style={{ width: "50%" }}>
+                {selector.map((e) => {
+                    return <>
+                        <Card sx={{ margin: "10px", display: 'flex' }}>
+                            <img src={e.images[0]} alt="image" style={{ border: "2px solid #ddd", borderRadius: '5', padding: "5px", maxWidth: "100px", minWidth: "100px", maxHeight: "100px", minHeight: "100px", marginTop: "auto", marginBottom: "auto" }} />
 
-                    <Card sx={{ margin: "10px", display: 'flex' }}>
-                        <img src={e.images[0]} alt="image" style={{ border: "2px solid #ddd", borderRadius: '5', padding: "5px", maxWidth: "100px", minWidth: "100px", maxHeight: "100px", minHeight: "100px", marginTop: "auto", marginBottom: "auto" }} />
+                            <CardContent>
+                                <Typography variant="h6" >
+                                    {e.title}
+                                </Typography>
 
+                                <div style={{ display: 'flex' }}>
+                                    <Typography variant="overline" >Size:​{e.sizes}</Typography>
+                                </div>
+
+                                <Typography style={{ color: 'lightslategray' }}>₹​{e.original_price - e.discounted_price}</Typography>
+                                <button type='button' style={{ backgroundColor: 'rgba(245, 40, 145, 0.8)', color: 'white', border: 'none', borderRadius: '5px', padding: '5px', marginTop: '10px' }} onClick={() => rem_from_cart(e.id)} >Remove</button>
+                            </CardContent>
+                        </Card>
+                    </>
+                })}
+            </div>
+            <div style={{ margin: 10, width: '30%' }}>
+                {selector.length > 0 ? <>
+                    <Card >
                         <CardContent>
-                            <Typography variant="h6" >
-                                {e.title}
-                            </Typography>
-
-                            <div style={{ display: 'flex' }}>
-                                <Typography variant="overline" >Size:​{e.sizes}</Typography>
-                            </div>
-
-                            <Typography style={{ color: 'lightslategray' }}>₹​{e.original_price - e.discounted_price}</Typography>
-                            <button type='button' style={{ backgroundColor: '#ff0000', color: 'white', border: 'none', borderRadius: '5px', padding: '5px', marginTop: '10px' }} onClick={() => rem_from_cart(e.id)} >Remove</button>
+                            <Typography variant="h6" display='block'>Price Details</Typography>
+                            <Typography variant='overline' display='block' >Product Charge : ₹{totalOriginalPrice}</Typography>
+                            <Typography variant='overline' display='block' >Discounted : ₹​{discounted_price}</Typography>
+                            <Typography variant='overline' display='block' >Total Amount : ₹{totalOriginalPrice - discounted_price}</Typography>
                         </CardContent>
                     </Card>
-                </>
-
-            })}
-
-
+                </> : <></>}
+            </div>
         </div>
-        <div> total is{total}</div>
     </>
 }
 
